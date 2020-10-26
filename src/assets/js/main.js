d884d63d1daeb2e17dd47363e572b214052c8dd8
Armen <armen.armen.armen95@gmail.com>
2020-10-26 16:52:08 +0200

    minor edits

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,7 @@ $(document).on("readystatechange", () => {
     }
 })
 $(window).on("load", () => {
+    let rightAnswer = 0
     // ----------------------------------------------
     // universal function
     function toggleModal() {
@@ -13,9 +14,72 @@ $(window).on("load", () => {
             $(".modal__content-video").trigger("play")
         else $(".modal__content-video").trigger("pause")
     }
+    function removeDisableButton() {
+        $(this)
+            .parents(".test__slide")
+            .find(".slide-button--disable")
+            .removeClass("slide-button--disable")
+    }
     //  /universal function
     // ----------------------------------------------
     // event
+    $(".test__slide-answer").on("click", function () {
+        if (!$(this).hasClass("test__slide-answer--open"))
+            removeDisableButton.bind(this)()
+        else
+            $(this)
+                .parents(".test__slide")
+                .find(".slide-button--next")
+                .addClass("slide-button--disable")
+    })
+    $(".slide-button--next").on("click", () => {
+        let buffRightAnswer = 0
+        $("input[type=radio]:not(.switch):checked").each(function () {
+            if ($(this).data("right")) buffRightAnswer++
+        })
+        rightAnswer = buffRightAnswer
+    })
+    $(".slide-button--last").on("click", () => {
+        const SELECTOR_LAST_TITLE = $(".test__slide-title--finish")
+        const RESULT_TEST =
+            +SELECTOR_LAST_TITLE.data("win-count") <= rightAnswer
+                ? "win"
+                : "lose"
+        SELECTOR_LAST_TITLE.text(
+            SELECTOR_LAST_TITLE.data(`${RESULT_TEST}-title`)
+                .replace("&&", rightAnswer)
+                .replace("||", SELECTOR_LAST_TITLE.data("count"))
+        )
+        SELECTOR_LAST_TITLE.parents(".test__slide")
+            .find(".test__slide-text")
+            .text(SELECTOR_LAST_TITLE.data(`${RESULT_TEST}-text`))
+        if ($(window).width() >= 700)
+            $(".test__slide-finish-picture-img").prop(
+                "src",
+                SELECTOR_LAST_TITLE.data(`${RESULT_TEST}-img`)
+            )
+        else {
+            $(".test__slide-finish-picture source").prop(
+                "srcset",
+                SELECTOR_LAST_TITLE.data(`${RESULT_TEST}-img-mobail`)
+            )
+        }
+    })
+    $(".test__slide-textarea").on("click", function () {
+        $(this)
+            .parents(".test__slide-answer")
+            .find(".test__slide-input")
+            .prop("checked", true)
+    })
+    $(".test__slide-textarea").on("blur", function () {
+        if ($(this).val()) removeDisableButton.bind(this)()
+    })
+    $(".test__slide-textarea").on("input", function () {
+        if ($(this).val()) removeDisableButton.bind(this)()
+    })
+    $(".slide-button").on("click", (e) => {
+        e.preventDefault()
+    })
     $(".menu__burger").on("click", () => {
         $(".menu").toggleClass("menu--open")
     })
@@ -37,4 +101,27 @@ $(window).on("load", () => {
     })
     // /event
     // ----------------------------------------------
+
+    // Tests
+
+    new Swiper(".test__swiper-container", {
+        spaceBetween: 48,
+        slidesPerView: 1,
+        on: {
+            sliderMove: function (e) {
+                e.preventDefault()
+            },
+        },
+        navigation: {
+            nextEl: ".slide-button--next",
+            prevEl: ".slide-button--prev",
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            701: {
+                slidesPerView: "auto",
+            },
+        },
+    })
+    // /Tests
 })
